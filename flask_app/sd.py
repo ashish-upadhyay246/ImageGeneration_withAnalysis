f506f1fcaf7626591961154bb142b2e730a63a3c
Ashish Upadhyay <upadhyay.ashish246@gmail.com>
2024-10-22 15:39:34 +0530

    Code Organization

--- flask_app/sd.py
+++ flask_app/sd.py
@@ -9,15 +9,17 @@ def stable_diff(prompt, num_inference_steps, guidance_scale, h, w):
     try:
         if not prompt.strip():
             return {"error": "Invalid prompt. The prompt cannot be empty."}
-        
         if h <= 0 or w <= 0:
             return {"error": "Invalid dimensions. Height and width must be greater than zero."}
 
-        device = "cuda" if torch.cuda.is_available() else "cpu"
-        model_id = "stable-diffusion-v1-5/stable-diffusion-v1-5"
-        pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
-        pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
-        pipe = pipe.to(device)
+        try:
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+            model_id = "stable-diffusion-v1-5/stable-diffusion-v1-5"
+            pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+            pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+            pipe = pipe.to(device)
+        except Exception as e:
+            return {"error": f"Failed to load Stable Diffusion model: {str(e)}"}
 
         with autocast("cuda"):
             image = pipe(
@@ -40,9 +42,8 @@ def stable_diff(prompt, num_inference_steps, guidance_scale, h, w):
             image.save(buffered, format="JPEG")
             generated_image_b64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
         except Exception as e:
-            return {"error": f"Failed to convert image to base64: {str(e)}"}
+            return {"error": f"Failed to convert image to base64 string: {str(e)}"}
 
         return generated_image_b64
-
     except Exception as e:
         return {"error": f"An error occurred: {str(e)}"}
