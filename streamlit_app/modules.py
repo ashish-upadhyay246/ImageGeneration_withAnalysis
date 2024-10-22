3021d83a9e1a647a102b2b90720f4977d5bc267f
Ashish Upadhyay <upadhyay.ashish246@gmail.com>
2024-10-22 15:22:24 +0530

    Updated modules.py

--- streamlit_app/modules.py
+++ streamlit_app/modules.py
@@ -12,25 +12,19 @@ def plot_polygons(polygons, generated_image):
         cv2.fillPoly(polygon_image, [np.array(polygon, np.int32)], color=(0, 255, 0, 128))
         cv2.polylines(polygon_image, [np.array(polygon, np.int32)], isClosed=True, color=(255, 255, 255), thickness=2)
         
-        #converting the image to rgb to display on streamlit
         polygon_image_rgb = cv2.cvtColor(polygon_image, cv2.COLOR_BGR2RGB)
         st.image(polygon_image_rgb, caption=f"Polygon {i + 1} (Coordinates: {polygon})", use_column_width=False, width=512)
 
 def plot_masks(masks, generated_image):
     st.subheader("Masks created on the original image")
-    #converting original image to format compatible with opencv
     original_image = cv2.cvtColor(np.array(generated_image), cv2.COLOR_RGB2BGR)
-
     overlay_image = original_image.copy()
-
     for i, mask in enumerate(masks):
         mask_uint8 = np.array(mask, dtype=np.uint8) * 255
-        #generate random colors for the masks
         random_color = np.random.randint(0, 256, size=3).tolist()
         #overlay the colored masks on the original image
-        overlay_image[mask_uint8 > 0] = [random_color[0], random_color[1], random_color[2]] #setting pixels of the mask to random colors
+        overlay_image[mask_uint8 > 0] = [random_color[0], random_color[1], random_color[2]]
 
-    #display original image with all masks overlayed
     annotated_image_rgb = cv2.cvtColor(overlay_image, cv2.COLOR_BGR2RGB)
     st.image(annotated_image_rgb, use_column_width=False, width=512)
 
